a880daf017a423131bdeacae6d02c46f5cd25060
Administrator <Administrator@AGOC3-704040029.lan>
2017-04-20 01:04:48 +0800

    123

---
+++ SpringMybaitsDemo/src/test/java/SpringconfigTest.java
@@ -0,0 +1,15 @@
+//import org.apache.log4j.Logger;
+//import org.junit.Test;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+//
+//public class SpringconfigTest {
+//	Logger logger=Logger.getLogger(SpringconfigTest.class);
+//	@Test
+//	public void test() {
+//		logger.debug("寮�濮�");
+//		ApplicationContext ac=new ClassPathXmlApplicationContext("applicationContext.xml");
+//		logger.debug("缁撴潫"+ac);
+//	}
+//
+//}
